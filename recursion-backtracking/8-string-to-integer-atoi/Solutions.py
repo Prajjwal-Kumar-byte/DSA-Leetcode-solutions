b67f776ad684c365dd8a384f5e517fda38d346a0
PRAJJWAL KUMAR <kumarprajjwal94@gmail.com>
2026-09-20 17:08:13 +0530

    Add myAtoi function for string to integer conversion
    Implement myAtoi function to convert string to integer with handling for whitespace, signs, and overflow.

--- recursion-backtracking/8-string-to-integer-atoi/Solutions.py
+++ recursion-backtracking/8-string-to-integer-atoi/Solutions.py
@@ -1,1 +1,33 @@
-
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        if not s:
+            return 0
+        index=0
+        l=len(s)
+        while index<l and s[index]==" ":
+            index+=1
+        sign=1
+        if index<l:
+            if s[index]=="-":
+                index+=1
+                sign=-1
+            elif s[index-1]=="+":
+                sign=1
+                index+=1
+        s=s[index:]
+        def parse(i,num):
+            if i==len(s) or not s[i].isdigit():
+                return num
+            digit=int(s[i])
+            new_num=num*10+digit
+            return parse(i+1,new_num)
+        num=parse(0,0)
+        if sign==-1:
+            num=num*sign
+        if num>2**31-1:
+            return 2**31-1
+        if num<-2**31:
+            return -2**31
+        return num
+S=Solution()
+print(S.myAtoi("123-"))
